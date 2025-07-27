38f5976d8e015ccb294cfd0ae7375c7f868feb54
KazApps <kazuki_shogi@outlook.jp>
2025-07-27 23:47:25 +0900

    Improve bonus update with rounding bench: 2501263

--- src/history.h
+++ src/history.h
@@ -86,7 +86,7 @@ class StatsEntry {
     void operator<<(int bonus) {
         // Make sure that bonus is in range [-D, D]
         int clampedBonus = std::clamp(bonus, -D, D);
-        entry += clampedBonus - entry * std::abs(clampedBonus) / D;
+        entry += clampedBonus - (entry * std::abs(clampedBonus) + D / 2) / D;
 
         assert(std::abs(entry) <= D);
     }
