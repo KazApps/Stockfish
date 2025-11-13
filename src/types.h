3649ab8ee7076c70a7fa37ae31d7fd3992def970
KazApps <kazuki_shogi@outlook.jp>
2025-11-13 11:32:47 +0900

    bench: 2626086

--- src/types.h
+++ src/types.h
@@ -295,20 +295,18 @@ struct DirtyPiece {
 struct DirtyThreat {
     DirtyThreat() { /* don't initialize data */ }
     DirtyThreat(Piece pc, Piece threatened_pc, Square pc_sq, Square threatened_sq, bool add) {
-        data = (add << 28) | (pc << 20) | (threatened_pc << 16) | (threatened_sq << 8) | (pc_sq);
+        data = pc << 17 | threatened_pc << 13 | threatened_sq << 7 | pc_sq << 1 | add;
     }
 
-    Piece  pc() const { return static_cast<Piece>(data >> 20 & 0xf); }
-    Piece  threatened_pc() const { return static_cast<Piece>(data >> 16 & 0xf); }
-    Square threatened_sq() const { return static_cast<Square>(data >> 8 & 0xff); }
-    Square pc_sq() const { return static_cast<Square>(data & 0xff); }
+    Piece  pc() const { return static_cast<Piece>(data >> 17 & 0xf); }
+    Piece  threatened_pc() const { return static_cast<Piece>(data >> 13 & 0xf); }
+    Square threatened_sq() const { return static_cast<Square>(data >> 7 & 0x3f); }
+    Square pc_sq() const { return static_cast<Square>(data >> 1 & 0x3f); }
     bool   add() const {
-        uint32_t b = data >> 28;
-        sf_assume(b == 0 || b == 1);
-        return b;
+        return data & 1;
     }
 
-   private:
+private:
     uint32_t data;
 };
 
