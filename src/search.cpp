873ee7e0633880bdfbd0ebb5da6772dbbe51a95f
KazApps <kazuki_shogi@outlook.jp>
2026-01-25 01:58:08 +0900

    bench: 2392740

--- src/search.cpp
+++ src/search.cpp
@@ -1223,6 +1223,10 @@ moves_loop:  // When in check, search starts here
         // Decrease/increase reduction for moves with a good/bad history
         r -= ss->statScore * 850 / 8192;
 
+        // Reduce reduction after a null move
+        if ((ss - 1)->currentMove == Move::null())
+            r -= 1024;
+
         // Scale up reductions for expected ALL nodes
         if (allNode)
             r += r / (depth + 1);
